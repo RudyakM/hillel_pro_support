ebf17b2b100dfa6a9cdb3ca813c5d79174561ac8
rudyak <rudyakmaxl@gmail.com>
2022-08-01 15:57:38 +0300

    change import

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from config.api import btc_usd, history, home
+from config.api.exchange_rates import btc_usd, history, home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
